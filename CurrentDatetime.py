612d11eea5c02e26c565734828371b91e9652044
DCI <badara.jammeh@hotmail.com>
2022-09-21 22:37:12 +0200

    added

--- CurrentDatetime.py
+++ CurrentDatetime.py
@@ -6,3 +6,16 @@ current = datetime.now()
 
 req_format = datetime.strftime(current, "%Y/%m/%d %H/%M/%S")
 print(req_format)
+
+##
+
+from datetime import datetime
+
+# datetime object containing current date and time
+now = datetime.now()
+ 
+print("now =", now)
+
+# dd/mm/YY H:M:S
+dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+print("date and time =", dt_string)	
---
+++ datetime_obj.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+my_date_string = "Mar 11 2011 11:31AM"
+
+datetime_object = datetime.strptime(my_date_string, '%b %d %Y %I:%M%p' )
+
+print(type(datetime_object))
+print(datetime_object)
+
+#%b is for Month as locale’s abbreviated name. Like Jan, Feb, …, Dec
+
+#%B is for Month as locale’s full name. Like January, February, …, December
